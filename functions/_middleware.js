370dcd542fac6f45b45a3f928de36d0ffd702fa6
Lsworkshop <78817372+Lsworkshop@users.noreply.github.com>
2026-02-28 19:41:35 -0800

    Add files via upload

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,16 @@
 export async function onRequest(context) {
-  const userAgent = context.request.headers.get("user-agent") || "";
+  const request = context.request;
+  const url = new URL(request.url);
+  const userAgent = request.headers.get("user-agent") || "";
 
   const isMobile = /iphone|android|mobile/i.test(userAgent);
 
-  if (isMobile) {
-    return Response.redirect("https://cfsapp.finovawm.com", 302);
+  // 防止已在 app 域名上时重复跳转
+  if (isMobile && !url.hostname.includes("cfsapp")) {
+    const redirectUrl = new URL(url.toString());
+    redirectUrl.hostname = "cfsapp.lsfinova.com";
+
+    return Response.redirect(redirectUrl.toString(), 302);
   }
 
   return context.next();
